f0c58852861bc39a28abe6e42a3fc5029d5e383f
pavin <fellipe306@gmail.com>
2025-11-02 21:22:49 -0300

    main page fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
+	const [expenses, setExpenses] = useState([]);
+
+	useEffect(() => {
+		const fetchExpenses = async () => {
+			try {
+				const response = await axios.get("http://localhost:3000/expenses");
+
+				setExpenses(response.data);
+			} catch (error) {
+				console.error("Error fetching expenses.", error);
+			}
+		};
+
+		fetchExpenses();
+	}, []);
+
 	return (
 		<div>
 			<h1>Expenses Analyzer</h1>
 			<h2>My Expenses</h2>
 			<ul>
-				<li>Item 1</li>
-				<li>Item 2</li>
+				{expenses.map((expense) => (
+					<li key={expense.id}>
+						<strong>{expense.description}</strong>
+						R$ {expense.amount} -
+						<em>({expense.category || "Category undefined"})</em>
+					</li>
+				))}
 			</ul>
 		</div>
 	);
