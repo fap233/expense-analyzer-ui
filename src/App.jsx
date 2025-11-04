70dbc9b82b865173b610db7622c6cdd517072a3f
pavin <fellipe306@gmail.com>
2025-11-04 12:11:08 -0300

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,50 @@ import React, { useEffect, useState } from "react";
 function App() {
 	const [expenses, setExpenses] = useState([]);
 
+	const [description, setDescription] = useState("");
+	const [amount, setAmount] = useState("");
+	const [category, setCategory] = useState("");
+
+	const fetchExpenses = async () => {
+		try {
+			const response = await axios.get("http://localhost:3000/expenses");
+
+			setExpenses(response.data);
+		} catch (error) {
+			console.error("Error fetching expenses.", error);
+		}
+	};
+
 	useEffect(() => {
-		const fetchExpenses = async () => {
-			try {
-				const response = await axios.get("http://localhost:3000/expenses");
-
-				setExpenses(response.data);
-			} catch (error) {
-				console.error("Error fetching expenses.", error);
-			}
-		};
-
 		fetchExpenses();
 	}, []);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		if (!description || !amount) {
+			alert("Description and value are required!");
+			return;
+		}
+
+		try {
+			const newExpense = {
+				description,
+				amount: parseFloat(amount),
+				category,
+			};
+
+			await axios.post("http://localhost:3000/expenses", newExpense);
+
+			setDescription("");
+			setAmount("");
+			setCategory("");
+
+			fetchExpenses();
+		} catch (error) {
+			console.error("Error adding expense:", error);
+		}
+	};
 
 	return (
 		<div>
